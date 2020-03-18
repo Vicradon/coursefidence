439298f105159f117035bcc09781077f6efc2cb6
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-03-18 15:47:52 +0100

    Pusing to master

--- lib/pages/courses.dart
+++ lib/pages/courses.dart
@@ -63,31 +63,83 @@ class CourseFormState extends State<CourseForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          RaisedButton(
-            onPressed: () {
-              // Validate returns true if the form is valid, otherwise false.
-              if (_formKey.currentState.validate()) {
-                // If the form is valid, display a snackbar. In the real world,
-                // you'd often call a server or save the information in a database.
-
-                Scaffold.of(context)
-                    .showSnackBar(SnackBar(content: Text('Processing Data')));
-              }
-            },
-            child: Text('Submit'),
-          ),
-        ],
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.book),
+                hintText: 'Course Name',
+                labelText: 'Name *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter the course name';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.confirmation_number),
+                hintText: 'Course Units',
+                labelText: 'Units *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter the course unit';
+                }
+                return null;
+              },
+            ),
+            // ConfidenceRangeSlide(),
+            
+            RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+/*
+class ConfidenceRangeSlide extends StatefulWidget {
+  @override
+  RangeSlideState createState() => RangeSlideState();
+}
+*/
+/*
+class RangeSlideState extends State<ConfidenceRangeSlide> {
+  RangeValues _values = RangeValues(0.3, 0.7);
+
+  @override
+  Widget build(BuildContext context) {
+    return RangeSlider(
+      values: _values,
+      min: 0,
+      max: 100,
+      onChanged: null,
+      divisions: 100,
+      onChangeStart: (RangeValues values) {
+        setState(() {
+          _values = values;
+        });
+      },
+      // onChanged: (RangeValues values) {
+      //   setState(() {
+      //     _values = values;
+      //   });
+      // },
+    );
+  }
+}
+
+*/
