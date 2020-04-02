f7d1f08a44449b246abfd0fabdc949b85f8f6c55
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-04-02 08:37:52 +0100

    Moving to dev1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:coursefidence/pages/auth.dart';
+import 'package:coursefidence/pages/auth.dart';
 import 'package:coursefidence/pages/courses.dart';
-import 'package:coursefidence/pages/overview.dart';
+// import 'package:coursefidence/pages/overview.dart';
 import 'package:provider/provider.dart';
 import 'package:coursefidence/utils/course_model.dart';
 
@@ -15,8 +15,8 @@ class App extends StatelessWidget {
       child: MaterialApp(
         initialRoute: '/',
         routes: {
-          // '/': (BuildContext context) => Auth(),
-          '/': (BuildContext context) => Overview(),
+          '/': (BuildContext context) => Auth(),
+          // '/': (BuildContext context) => Overview(),
           '/courses': (BuildContext context) => Courses(),
         },
       ), // theme: Theme(data: ThemeData(accentColor: Color(Colors.orange)), child: null),
